6017b1830f1f7498678f7fe91c3ce1d551ae1828
Stegano <stegano@naver.com>
2017-10-02 01:11:49 +0900

    Add Tests: Event listener. - Add Event listener. - Existing code has been refactored.

--- test/Memoize.test.js
+++ test/Memoize.test.js
@@ -1,99 +1,134 @@
 var Memoize = require("../src/index");
 var expect = require("chai").expect;
-describe("Memoize", () => {
+
+describe("Test the basic functionality", () => {
   var cache = null;
-  it("Constructor test", () => {
+  it("constructor", () => {
     cache = new Memoize();
     var expected = Memoize;
     expect(cache).to.be.instanceOf(expected);
   });
-  it("Execute test", () => {
-    var execute = cache.execute("Execute test", () => {
+  it("execute", (done) => {
+    cache.execute("#", () => {
       return new Promise(resolve => {
+        resolve("Execution result");
       });
+    }).then((value) => {
+      var expected = "Execution result";
+      expect(value).to.be.equal(expected);
+      done();
     });
-    expect(execute).to.be.instanceOf(Promise);
-    expect(cache.execute.bind(null, "Execute Test", () => {
-      return "Not Promise";
-    })).to.throw();
   });
-  it("Hit test", done => {
-    // 1, - update
-    cache.execute("hitTest", () => {
-      return new Promise(resolve => {
-        resolve(1);
-      });
-    }).then(v => {
-      expect(v).to.be.equal(1);
-    });
-    // 2, - hit
-    setTimeout(() => {
-      cache.execute("hitTest", () => {
+});
+
+describe("Test the cache", () => {
+  var cache = new Memoize({
+    expiredTime: 50,
+    graceful: false
+  });
+  it("cache hit", (done) => {
+    // The cache key is `#`
+    cache
+      .execute("#", () => {
         return new Promise(resolve => {
+          // The cache value is `1`.
           resolve(1);
         });
-      }).then(v => {
-        expect(v).to.be.equal(1);
       });
-    }, 1000);
-    // 3, - update
     setTimeout(() => {
-      cache.execute("hitTest", () => {
-        return new Promise(resolve => {
-          resolve(2);
+      cache
+        .execute("#", () => {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              // After 50ms this value is cached and the result is notify to the Promise object.
+              resolve(2);
+            }, 50);
+          });
+        })
+        .then((value) => {
+          // It runs after 50ms.
+          var expected = 2;
+          expect(value).to.be.equal(expected);
+          done();
         });
-      }).then(v => {
-        expect(v).to.be.equal(2);
-        done();
-      });
-    }, 4000);
-  }).timeout(5000 * 10);
-  it("Graceful test", done => {
-    cache = new Memoize({
-      graceful: true,
-      expiredTime: 1000
-    });
-    // 1, - update
-    cache.execute("gracefulTest", () => {
-      return new Promise(resolve => {
-        resolve(1);
-      });
-    }).then(v => {
-      expect(v).to.be.equal(1);
-    });
-    // 2, - hit
-    setTimeout(() => {
-      cache.execute("gracefulTest", () => {
+    }, 60);
+  });
+});
+
+describe("Test the graceful", () => {
+  var cache = new Memoize({
+    expiredTime: 50,
+    graceful: true
+  });
+  it("cache hit", (done) => {
+    // The cache key is `#`
+    cache
+      .execute("#", () => {
         return new Promise(resolve => {
+          // The cache value is `1`.
           resolve(1);
         });
-      }).then(v => {
-        expect(v).to.be.equal(1);
       });
-    }, 1500);
-    // 3, - gracefully update
     setTimeout(() => {
-      cache.execute("gracefulTest", () => {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(2);
-          }, 100);
+      cache
+        .execute("#", () => {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              // After 50ms this value is cached and the result is notify to the Promise object.
+              resolve(2);
+            }, 50);
+          });
+        })
+        .then((value) => {
+          // Execute immediately with cached values, and cache new execution results.
+          var expected = 1;
+          expect(value).to.be.equal(expected);
+          done();
         });
-      }).then(v => {
-        expect(v).to.be.equal(1);
-        done();
-      });
-    }, 3500);
-    // 4, - update
+    }, 60);
+  });
+});
+
+describe("Test the events", () => {
+  var cache = new Memoize({
+    expiredTime: 50
+  });
+  it("Tests cached event", done => {
+    cache.on("cached", (value) => {
+      var expected = Promise;
+      expect(value).to.be.instanceof(expected);
+      done();
+    });
+    cache.execute("test", () => {
+      return new Promise(resolve => {
+        resolve(1);
+      })
+    });
+  });
+  it("Tests hit event", done => {
+    cache.on("hit", (value) => {
+      expect(value).to.be.equal(1);
+      done();
+    });
+    // The previously executed result will be called.
+    cache.execute("test", () => {
+      return new Promise(resolve => {
+        resolve(1);
+      })
+    });
+  });
+  it("Tests refresh event", done => {
+    cache.on("refresh", (value) => {
+      var expected = Promise;
+      expect(value).to.be.instanceof(expected);
+      done();
+    });
     setTimeout(() => {
-      cache.execute("gracefulTest", () => {
+      cache.execute("test", () => {
         return new Promise(resolve => {
-          resolve(2);
-        });
-      }).then(v => {
-        expect(v).to.be.equal(2);
-        done();
+          resolve(1);
+        })
       });
-    }, 4000);
-  }).timeout(5000 * 10);
+    }, 50);
+  });
 });
