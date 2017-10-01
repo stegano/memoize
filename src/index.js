978dd7ea9ae084946ee34c6dd802952b329aa8b8
Stegano <stegano@naver.com>
2017-10-02 01:07:38 +0900

    Add: Event listener. - `hit`, `refresh`, `cached` events has been added.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+var Events = require("events");
 /**
  * @class Memoize
  * @param {Object} settings Global settings.
  * */
 var Memoize = function (settings) {
   var cache = {};
+  var eventEmitter = new Events();
+  var eventList = [
+    "hit",
+    "cached",
+    "refresh"
+  ];
   var _settings = settings || {};
   _settings.expiredTime = Number(_settings.expiredTime) || 3 * 1000;
   _settings.graceful = typeof _settings.graceful === "boolean" ? _settings.graceful : false;
@@ -21,21 +28,59 @@ var Memoize = function (settings) {
     var ret = null;
     if (_isHit(key)) {
       ret = cache[key].data;
+      ret.then((value) => {
+        eventEmitter.emit("hit", value);
+      });
     } else {
       var executedFunc = yourFunction();
       if (!(executedFunc instanceof Promise)) {
         throw new TypeError("The result of executing the function must be Promise.");
       }
-      if (_graceful && cache[key] && cache[key].data) {
+      if (_graceful && key in cache && cache[key].data) {
         ret = cache[key].data;
         cache[key] = _genCacheItem(executedFunc, _expiredTime);
+        eventEmitter.emit("refresh", ret);
       } else {
         ret = executedFunc;
-        cache[key] = _genCacheItem(ret, _expiredTime);
+        if (key in cache) {
+          eventEmitter.emit("refresh", ret);
+        } else {
+          eventEmitter.emit("cached", ret);
+        }
+        cache[key] = _genCacheItem(executedFunc, _expiredTime);
       }
     }
     return ret;
   };
+
+  /**
+   * @memberOf Memoize
+   * @param {String} eventName
+   * @param {Function} callbackFunction
+   * */
+  this.on = (eventName, callbackFunction) => {
+    var ret = false;
+    if (eventList.includes(eventName)) {
+      eventEmitter.on(eventName, callbackFunction);
+      ret = true;
+    }
+    return ret;
+  };
+
+  /**
+   * @memberOf Memoize
+   * @param {String} eventName
+   * @param {Function} callbackFunction
+   * */
+  this.off = (eventName, callbackFunction) => {
+    var ret = false;
+    if (ret = eventList.includes(eventName)) {
+      eventEmitter.removeEventListener(eventName, callbackFunction);
+      ret = true;
+    }
+    return ret;
+  };
+
   /**
    * @memberOf Memoize
    * @private
@@ -67,4 +112,5 @@ var Memoize = function (settings) {
     };
   }
 };
+
 module.exports = Memoize;
